1c656a0b0b3c7418c91c72ea544fa4dc4ce9aaba
HannahNCI <44295285+HannahNCI@users.noreply.github.com>
2020-10-27 16:45:07 +0100

    Update verification.php

--- verification.php
+++ verification.php
@@ -4,8 +4,7 @@
 				<h3><strong>You're ready to go!</strong></h3>
 				<br />
 				<h5>Hi, <?php echo $_REQUEST['firstname']." ".$_REQUEST['lastname']?><h5>
-				<h5>We've finished setting up your account.<h5>
-				<h5>We sent you a confirmation to your email account<h5>
+				<h5>We sent you a confirmation to your email account, Please click to link in your email.<h5>
 				<a class="btn btn-primary" href="https://<?php echo $_REQUEST['email']?>" target="_blank">Confirm Email</a>
 			<?php
 				}
